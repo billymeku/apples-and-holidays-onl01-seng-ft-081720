58424263fc285f6cde38220e068cc6492e73f83f
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-24 23:52:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,8 +49,9 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter #season
   holiday_hash[:winter].collect do |holiday, supply|
-    supply
-  end.flatten
+    supply.flatten
+  end
+  
 end
 
 def all_supplies_in_holidays(holiday_hash)
