7a0016649ef69d3155dff442a06a959ade5ceb23
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-24 23:25:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].each do |key, value|
     value.each do |item|
       supplies << item
-    endn
+    end
 end
 
 def all_supplies_in_holidays(holiday_hash)
